e7275254efb17f767f38f4b8d1a3fc8960ea8a61
mohammedkhajafareeduddin <khaja253@gmail.com>
2021-02-28 11:05:26 -0500

    added mat tool bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { SearchComponent } from './search/search.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { ShowDetailsComponent } from './show-details/show-details.component';
 import { ShowInfoComponent } from './show-info/show-info.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { ShowInfoComponent } from './show-info/show-info.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
