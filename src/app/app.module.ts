dd346684b739e96195f56bcc90c778e43f9d2b2c
mohammedkhajafareeduddin <khaja253@gmail.com>
2021-03-02 11:09:06 -0500

    got an error..cannot find

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,15 @@ import { SearchComponent } from './search/search.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { ShowDetailsComponent } from './show-details/show-details.component';
 import { ShowInfoComponent } from './show-info/show-info.component';
+
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
 import {FormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { WatchlistComponent } from './watchlist/watchlist.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -21,7 +26,8 @@ import {MatIconModule} from '@angular/material/icon';
     SearchComponent,
     SearchResultsComponent,
     ShowDetailsComponent,
-    ShowInfoComponent
+    ShowInfoComponent,
+    WatchlistComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +36,9 @@ import {MatIconModule} from '@angular/material/icon';
     MatToolbarModule,
     FormsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
