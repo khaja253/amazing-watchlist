51809ce1425c1738073adda7e04659fe80571a10
mohammedkhajafareeduddin <khaja253@gmail.com>
2021-04-26 17:24:44 -0400

    quiz_9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchComponent } from './search/search.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { ShowInfoComponent } from './show-info/show-info.component';
 import { ShowDetailsComponent } from './show-details/show-details.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
+import { ErrorComponent } from './error/error.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
 import { MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
-import { ErrorComponent } from './error/error.component';
-import {DummyService} from './services/dummy.service';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { EncryptPipe } from './controls/encrypt.pipe';
+import { ShortenPipe } from './controls/shorten.pipe';
 
 @NgModule({
   declarations: [
@@ -30,20 +33,23 @@ import {DummyService} from './services/dummy.service';
     ShowInfoComponent,
     ShowDetailsComponent,
     WatchlistComponent,
-    DummyService,
-    ErrorComponent
+    ErrorComponent,
+    EncryptPipe,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FlexLayoutModule,
     MatToolbarModule,
     MatInputModule,
     FormsModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
-    MatTableModule
+    MatTableModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
